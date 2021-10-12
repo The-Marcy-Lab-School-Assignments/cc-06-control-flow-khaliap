44040965b4af3551d2f8206294fca080c4bb016a
khalia <khaliaparris01@gmail.com>
2021-10-12 14:23:04 +0000

    completed questions 1 and 2 of code challenge

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 //1
 function sumOfThreeAndFive(){
-  
+    let sum = 0
+  for (let i = 0; i < 1000; i ++){
+      if (i%3 === 0 || i%5 === 0)
+     sum += i
+  } return sum
 }
+// console.log(sumOfThreeAndFive())
 
-function greaterNum(){
+function greaterNum(num1, num2){
+    if ( num1 === num2){
+        return "both integers are equal"
+    }else if (typeof num1 !== "number" || typeof num2 !== "number") {
+        return null
+    }else {
+        return Math.max(num1, num2)
+    }
   
 }
+console.log(greaterNum(12,"12"))
 
 function oddAndEvenInFifteen(){
   
